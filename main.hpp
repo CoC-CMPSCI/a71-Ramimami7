87d605ca9250293ce10410253ab55f630283020a
Ramimami7 <rhabbal22@gmail.com>
2026-05-12 10:11:35 -0700

    main.cpp

--- main.hpp
+++ main.hpp
@@ -11,10 +11,32 @@ const int SIZE = 100;
 
 int makearray(int number[])
 {
+
+    srand(time(0));
+
+    int size = rand() % 11 + 10;
+ 
+    for (int i = 0, i < size; i++){
+        number[i] = rand() % 100; 
+    }
+    return size;
 }
 void bubble(int number[], int last)
 {
+    for (int i = 0; i < N - 1; i++) {
+        if number[i] > number[i + 1] {
+            int temp = number[i];
+            number[i] = number[i + 1];
+            number[i + 1] = temp;
+        }
+    }
 }
+
 void printout(int number[], int last)
 {
+    for (int i = 0; i < N; i++){
+        cout << number[i] << "\t";
+    }
+    cout << endl;
 }
+
